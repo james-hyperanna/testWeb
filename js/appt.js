f3d6488048b7e214c155c6156cfb4eb4e3264b16
james-hyperanna <james@hyperanna.com>
2018-02-02 16:08:52 +1100

    add tables dynamically (need to move Dr.Hong as well)

--- js/appt.js
+++ js/appt.js
@@ -27,10 +27,109 @@ $(function() {
   $('#datepicker').datepicker({
     dateFormat: "dd-mm-yy"
   }).datepicker("setDate", "0");
+
+  var doctors = ["Dr.Joo", "Dr.McDonald"];
+  drawTimeTables(doctors);
 });
 
+// draw time tables dynamically
+function drawTimeTables(doctors) {
+  for (var i = 0; i < doctors.length; i++) {
+    $('#time-tables').append(
+      '<div class="doctor-container" style="margin-top: 50px;"><h4 class="doctor-name">' + doctors[i] + '</h4>\n' +
+      '        <table>\n' +
+      '            <tr>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>9:00am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>9:30am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>10:00am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>10:30am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '            </tr>\n' +
+      '            <tr>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>11:00am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>11:30am</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>12:00pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>12:30pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '            </tr>\n' +
+      '            <tr>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>1:00pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>1:30pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>2:00pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>2:30pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '            </tr>\n' +
+      '            <tr>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>3:00pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>3:30pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>4:00pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '                <td class="btn btn-info btn-lg appointment-time">\n' +
+      '                    <div>4:30pm</div>\n' +
+      '                    <div></div>\n' +
+      '                </td>\n' +
+      '            </tr>\n' +
+      '        </table>\n' +
+      '</div>')
+  }
+}
+
 // open
+/* does not work with the appended htmls
 $('.appointment-time').click(function() {
+  console.log($(this).closest('.doctor-container').find('.doctor-name').text());
+  // $('#appointment-doctor').text($(this).closest('.doctor-name').text()); // get the doctor
+  // $('#appointment-date').text($('#datepicker').val()); // get the date
+  // $('#appointment-time').text($(this).children(':first').text()); // get the time
+  // $('#myModal').modal('show'); // open the popup
+});
+*/
+
+//
+$('#time-tables').on('click', '.appointment-time', function() {
+  // console.log($(this).closest('.doctor-container').find('.doctor-name').text());
+  $('#appointment-doctor').text($(this).closest('.doctor-name').text()); // get the doctor
   $('#appointment-date').text($('#datepicker').val()); // get the date
   $('#appointment-time').text($(this).children(':first').text()); // get the time
   $('#myModal').modal('show'); // open the popup
