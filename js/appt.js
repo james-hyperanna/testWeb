41380778f6c0781448d1b8407b3fa2715c9ae9df
james-hyperanna <james@hyperanna.com>
2018-02-07 15:52:17 +1100

    grab exisiting appointments under a doctor on a certain date

--- js/appt.js
+++ js/appt.js
@@ -13,9 +13,23 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var selectedDoc;
 
-function makeAppt(doctor, date, time, name) {
-  console.log(database);
+// set today's date by default
+$(function() {
+  $('#datepicker').datepicker({
+    dateFormat: "dd-mm-yy"
+  }).datepicker("setDate", "0");
 
+  var doctors = ["DrHong", "DrJoo", "DrMcDonald"];
+  var availableTimes = ["9:00am", "9:30am", "10:00am", "10:30am",
+    "11:00am", "11:30am", "12:00pm", "12:30pm",
+    "1:00pm", "1:30pm", "2:00pm", "2:30pm",
+    "3:00pm", "3:30pm", "4:00pm", "4:30pm"]
+  drawTimeTables(doctors, availableTimes);
+  loadAppt(doctors[0], "02-02-2018");
+});
+
+
+function makeAppt(doctor, date, time, name) {
   database.ref('appointments/' + doctor + "/" + date + "/" + time).set({
     date: date,
     time: time,
@@ -23,19 +37,11 @@ function makeAppt(doctor, date, time, name) {
   });
 }
 
-// set today's date by default
-$(function() {
-  $('#datepicker').datepicker({
-    dateFormat: "dd-mm-yy"
-  }).datepicker("setDate", "0");
-
-  var doctors = ["Dr.Hong", "Dr.Joo", "Dr.McDonald"];
-  var availableTimes = ["9:00am", "9:30am", "10:00am", "10:30am",
-                        "11:00am", "11:30am", "12:00pm", "12:30pm",
-                        "1:00pm", "1:30pm", "2:00pm", "2:30pm",
-                        "3:00pm", "3:30pm", "4:00pm", "4:30pm"]
-  drawTimeTables(doctors, availableTimes);
-});
+function loadAppt(doctor, date) {
+  database.ref('appointments/' + doctor + '/' + date).on('value', function(snapshot) {
+    console.log(JSON.stringify(snapshot.val(), null, 2));
+  })
+}
 
 // draw time tables dynamically
 function drawTimeTables(doctors, availableTimes) {
